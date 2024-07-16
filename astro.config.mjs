ff4962e5531725c34615897b0d235dea9b042623
abumalick <abdullah.hilson@gmail.com>
2024-07-16 22:12:55 +0300

    chore: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   integrations: [react()],
   output: "hybrid",
 });
